1d1559117f6e219fc7dd6824540024e98e7b4bd7
Leandro Simões <lesimoes0@gmail.com>
2025-04-05 16:29:30 -0300

    Initial Commit

---
+++ clip-save/clip_saveApp.swift
@@ -0,0 +1,17 @@
+//
+//  clip_saveApp.swift
+//  clip-save
+//
+//  Created by Leandro Simoes on 05/04/25.
+//
+
+import SwiftUI
+
+@main
+struct clip_saveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
